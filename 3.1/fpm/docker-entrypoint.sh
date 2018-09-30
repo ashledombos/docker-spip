ab5f7597f9b0c4ead73989be0aabc9359159a1f9
ashledombos <rj@hodo.fr>
2018-10-01 00:12:02 +0200

    many improvements

--- 3.1/fpm/docker-entrypoint.sh
+++ 3.1/fpm/docker-entrypoint.sh
@@ -13,30 +13,57 @@ echo 'upload_max_filesize=${PHP_UPLOAD_MAX_FILESIZE}'; \
 echo 'date.timezone=${PHP_TIMEZONE}'; \
 ) > /usr/local/etc/php/conf.d/spip.ini
 
-if ! [ -e index.php -a -e ecrire/inc_version.php ]; then
-	echo >&2 "SPIP not found in $PWD - copying now..."
-	if [ "$(ls -A)" ]; then
-		echo >&2 "WARNING: $PWD is not empty - press Ctrl+C now if this is an error!"
-		( set -x; ls -A; sleep 10 )
-	fi
-	tar cf - --one-file-system -C /usr/src/spip . | tar xf -
-	echo >&2 "Complete! SPIP has been successfully copied to $PWD"
+# Separate the core of SPIP and data
+# Copy SPIP core to shared volume.
+echo >&2 "Setting SPIP core in $PWD/core/"
+rm -Rf /var/www/html/core/*
+tar cf - --one-file-system -C /usr/src/spip . | tar xf - -C core
+echo >&2 "Complete! SPIP has been successfully copied to $PWD/core/"
 
-  echo >&2 "Create plugins, libraries and template directories"
-  mkdir -p plugins/auto; \
-    mkdir -p lib; \
-    mkdir -p squelettes; \
-		mkdir -p tmp/{dump,log,upload}; \
-		chown -R www-data:www-data plugins lib squelettes tmp
-
-	if [ ! -e .htaccess ]; then
-    cp htaccess.txt .htaccess
-		chown www-data:www-data .htaccess
-	fi
+# IMG and config will be modified so let's move it to data volume
+echo >&2 "Move IMG and config in $PWD/data/ if needed"
+if [ ! -d data/IMG ]; then
+  mv core/IMG data/
+else
+  rm -Rf core/IMG/
+fi
+if [ ! -d data/config ]; then
+  mv core/config data/
+else
+  rm -Rf core/config
 fi
 
+# For better performance, we include the content of htaccess.txt in apache conf
+echo >&2 "Apache uses $PWD/data/htaccess.txt instead of .htaccess"
+if [ ! -e data/htaccess.txt ]; then
+  mv core/htaccess.txt data/htaccess.txt
+else
+  rm core/htaccess.txt
+fi
+
+# All necessary directories should be created if they aren't yet
+echo >&2 "Create plugins, libraries and template directories in $PWD/data/ if they don't exist"
+mkdir -p data/plugins/auto
+mkdir -p data/lib
+mkdir -p data/squelettes
+
+echo >&2 "Populate $PWD/core/tmp directory"
+mkdir -p core/tmp/{dump,log,upload}
+
+echo >&2 "change rights"
+chown -R www-data:www-data data/IMG data/config data/plugins data/lib data/squelettes data/htaccess.txt core/tmp
+
+# As core directory is the webroot directory, we link all subdirectories from data volume
+echo >&2 "create all symlinks"
+ln -s $PWD/data/IMG core/
+ln -s $PWD/data/config core/
+ln -s $PWD/data/plugins core/
+ln -s $PWD/data/lib core/
+ln -s $PWD/data/squelettes core/
+ln -s $PWD/data/htaccess.txt core/
+
 # Install SPIP
-if [ ! -e config/connect.php ]; then
+if [ ! -e data/config/connect.php ]; then
 	# Wait for mysql before install
 	# cf. https://docs.docker.com/compose/startup-order/
 	if [ ${SPIP_VERSION} == '3.2' ] || [ ${SPIP_VERSION} == '3.1' ] || [ ${SPIP_VERSION} == '3.0' ]; then
@@ -47,7 +74,7 @@ if [ ! -e config/connect.php ]; then
 			done
 		fi
 
-		spip install \
+		(cd core && spip install \
 			--db-server ${SPIP_DB_SERVER} \
 			--db-host ${SPIP_DB_HOST} \
 			--db-login ${SPIP_DB_LOGIN} \
@@ -57,12 +84,13 @@ if [ ! -e config/connect.php ]; then
 			--admin-nom ${SPIP_ADMIN_NAME} \
 			--admin-login ${SPIP_ADMIN_LOGIN} \
 			--admin-email ${SPIP_ADMIN_EMAIL} \
-			--admin-pass ${SPIP_ADMIN_PASS}
+			--admin-pass ${SPIP_ADMIN_PASS} \
+        )
 
 		# Fix bug can't create admin account for SPIP 3.0 by retry SPIP install
 		# Need to fix spip-cli "mysql_query() expects parameter 2 to be resource"
 		if [ ${SPIP_VERSION} == '3.0' ]; then
-			spip install \
+			(cd spip install \
 				--db-server ${SPIP_DB_SERVER} \
 				--db-host ${SPIP_DB_HOST} \
 				--db-login ${SPIP_DB_LOGIN} \
@@ -72,7 +100,8 @@ if [ ! -e config/connect.php ]; then
 				--admin-nom ${SPIP_ADMIN_NAME} \
 				--admin-login ${SPIP_ADMIN_LOGIN} \
 				--admin-email ${SPIP_ADMIN_EMAIL} \
-				--admin-pass ${SPIP_ADMIN_PASS}
+				--admin-pass ${SPIP_ADMIN_PASS} \
+			)	
 		fi
 	fi
 
--- 3.2/fpm/docker-entrypoint.sh
+++ 3.2/fpm/docker-entrypoint.sh
@@ -13,11 +13,14 @@ echo 'upload_max_filesize=${PHP_UPLOAD_MAX_FILESIZE}'; \
 echo 'date.timezone=${PHP_TIMEZONE}'; \
 ) > /usr/local/etc/php/conf.d/spip.ini
 
+# Separate the core of SPIP and data
+# Copy SPIP core to shared volume.
 echo >&2 "Setting SPIP core in $PWD/core/"
 rm -Rf /var/www/html/core/*
 tar cf - --one-file-system -C /usr/src/spip . | tar xf - -C core
 echo >&2 "Complete! SPIP has been successfully copied to $PWD/core/"
 
+# IMG and config will be modified so let's move it to data volume
 echo >&2 "Move IMG and config in $PWD/data/ if needed"
 if [ ! -d data/IMG ]; then
   mv core/IMG data/
@@ -30,6 +33,7 @@ else
   rm -Rf core/config
 fi
 
+# For better performance, we include the content of htaccess.txt in apache conf
 echo >&2 "Apache uses $PWD/data/htaccess.txt instead of .htaccess"
 if [ ! -e data/htaccess.txt ]; then
   mv core/htaccess.txt data/htaccess.txt
@@ -37,6 +41,7 @@ else
   rm core/htaccess.txt
 fi
 
+# All necessary directories should be created if they aren't yet
 echo >&2 "Create plugins, libraries and template directories in $PWD/data/ if they don't exist"
 mkdir -p data/plugins/auto
 mkdir -p data/lib
@@ -48,6 +53,7 @@ mkdir -p core/tmp/{dump,log,upload}
 echo >&2 "change rights"
 chown -R www-data:www-data data/IMG data/config data/plugins data/lib data/squelettes data/htaccess.txt core/tmp
 
+# As core directory is the webroot directory, we link all subdirectories from data volume
 echo >&2 "create all symlinks"
 ln -s $PWD/data/IMG core/
 ln -s $PWD/data/config core/
@@ -57,7 +63,7 @@ ln -s $PWD/data/squelettes core/
 ln -s $PWD/data/htaccess.txt core/
 
 # Install SPIP
-if [ ! -e config/connect.php ]; then
+if [ ! -e data/config/connect.php ]; then
 	# Wait for mysql before install
 	# cf. https://docs.docker.com/compose/startup-order/
 	if [ ${SPIP_VERSION} == '3.2' ] || [ ${SPIP_VERSION} == '3.1' ] || [ ${SPIP_VERSION} == '3.0' ]; then
@@ -68,7 +74,7 @@ if [ ! -e config/connect.php ]; then
 			done
 		fi
 
-		spip install \
+		(cd core && spip install \
 			--db-server ${SPIP_DB_SERVER} \
 			--db-host ${SPIP_DB_HOST} \
 			--db-login ${SPIP_DB_LOGIN} \
@@ -78,12 +84,13 @@ if [ ! -e config/connect.php ]; then
 			--admin-nom ${SPIP_ADMIN_NAME} \
 			--admin-login ${SPIP_ADMIN_LOGIN} \
 			--admin-email ${SPIP_ADMIN_EMAIL} \
-			--admin-pass ${SPIP_ADMIN_PASS}
+			--admin-pass ${SPIP_ADMIN_PASS} \
+        )
 
 		# Fix bug can't create admin account for SPIP 3.0 by retry SPIP install
 		# Need to fix spip-cli "mysql_query() expects parameter 2 to be resource"
 		if [ ${SPIP_VERSION} == '3.0' ]; then
-			spip install \
+			(cd spip install \
 				--db-server ${SPIP_DB_SERVER} \
 				--db-host ${SPIP_DB_HOST} \
 				--db-login ${SPIP_DB_LOGIN} \
@@ -93,7 +100,8 @@ if [ ! -e config/connect.php ]; then
 				--admin-nom ${SPIP_ADMIN_NAME} \
 				--admin-login ${SPIP_ADMIN_LOGIN} \
 				--admin-email ${SPIP_ADMIN_EMAIL} \
-				--admin-pass ${SPIP_ADMIN_PASS}
+				--admin-pass ${SPIP_ADMIN_PASS} \
+			)	
 		fi
 	fi
 
