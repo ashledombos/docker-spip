11fdf4cc5b9f08370536b52257a5cb1cd1482183
ashledombos <rj@hodo.fr>
2019-05-03 20:46:14 +0200

    Update docker-entrypoint.sh

--- 3.2/fpm/docker-entrypoint.sh
+++ 3.2/fpm/docker-entrypoint.sh
@@ -4,13 +4,13 @@ set -e
 # Reconfigure php.ini
 # set PHP.ini settings for SPIP
 ( \
-echo 'display_errors=Off'; \
-echo 'error_log=/var/log/apache2/php.log'; \
-echo 'max_execution_time=${PHP_MAX_EXECUTION_TIME}'; \
-echo 'memory_limit=${PHP_MEMORY_LIMIT}'; \
-echo 'post_max_size=${PHP_POST_MAX_SIZE}'; \
-echo 'upload_max_filesize=${PHP_UPLOAD_MAX_FILESIZE}'; \
-echo 'date.timezone=${PHP_TIMEZONE}'; \
+echo "display_errors=Off"; \
+echo "error_log=/var/log/apache2/php.log"; \
+echo "max_execution_time=${PHP_MAX_EXECUTION_TIME}"; \
+echo "memory_limit=${PHP_MEMORY_LIMIT}"; \
+echo "post_max_size=${PHP_POST_MAX_SIZE}"; \
+echo "upload_max_filesize=${PHP_UPLOAD_MAX_FILESIZE}"; \
+echo "date.timezone=${PHP_TIMEZONE}"; \
 ) > /usr/local/etc/php/conf.d/spip.ini
 
 # Separate the core of SPIP and data
